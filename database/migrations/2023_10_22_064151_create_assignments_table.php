6fc9765ad279af5ffb3fc61148d6ba38ebac956a
Abdalhaqq Muhammad Saih <abdalhaqqm@gmail.com>
2023-10-26 15:05:29 +0700

    change deadline to string

--- database/migrations/2023_10_22_064151_create_assignments_table.php
+++ database/migrations/2023_10_22_064151_create_assignments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("title");
             $table->text("description");
-            $table->date("deadline");
+            $table->string("deadline");
             $table->timestamps();
         });
     }
